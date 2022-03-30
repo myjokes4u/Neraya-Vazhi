4a6ad71cddd8c1b42dc13b7d6762ed11f36b5a1c
Abdul Azeez M <myjokes4uazeez@gmail.com>
2022-03-30 20:25:54 +0300

    update 01 ls
    clear

--- script.js
+++ script.js
@@ -13,28 +13,45 @@ const post = {
     description: "വിവരണം മലയാളം",
     category: "",
     contents: "",
-    thumbnail: "/images/nv-icon.png",
+    thumbnail: "images/nv-icon.png",
     duration: "0:00:00 - MIN",
     size: "0.00 - MB",
     file: ""
 };
 
 $(document).ready(function(){
-$("#bismi").text(post.bismi);
-$("#dua").text(post.dua);
-$("#subject_ml").text(post.subject_ml);
-$("#subject_en").text(post.subject_en);
-$("#fSpeech_ml").text(post.fSpeech_ml);
-$("#fSpeech_en").text(post.fSpeech_en);
-$("#usthad_ml").text(post.usthad_ml);
-$("#usthad_en").text(post.usthad_en);
-$("#date").text(post.date);
-$("#place_ml").text(post.place_ml);
-$("#place_en").text(post.place_en);
-$("#description").text(post.description);
-$("#thumbnail").attr("src", post.thumbnail);
-$("#duration").text(post.duration);
-$("#size").text(post.size);
+$("#post #bismi").text(post.bismi);
+$("#post #dua").text(post.dua);
+$("#post #subject_ml").text(post.subject_ml);
+$("#post #subject_en").text(post.subject_en);
+$("#post #fSpeech_ml").text(post.fSpeech_ml);
+$("#post #fSpeech_en").text(post.fSpeech_en);
+$("#post #usthad_ml").text(post.usthad_ml);
+$("#post #usthad_en").text(post.usthad_en);
+$("#post #date").text(post.date);
+$("#post #place_ml").text(post.place_ml);
+$("#post #place_en").text(post.place_en);
+$("#post #description").text(post.description);
+$("#post #thumbnail").attr("src", post.thumbnail);
+$("#post #duration").text(post.duration);
+$("#post #size").text(post.size);
+
+$("#favorite #bismi").text(post.bismi);
+$("#favorite #dua").text(post.dua);
+$("#favorite #subject_ml").text(post.subject_ml);
+$("#favorite #subject_en").text(post.subject_en);
+$("#favorite #fSpeech_ml").text(post.fSpeech_ml);
+$("#favorite #fSpeech_en").text(post.fSpeech_en);
+$("#favorite #usthad_ml").text(post.usthad_ml);
+$("#favorite #usthad_en").text(post.usthad_en);
+$("#favorite #date").text(post.date);
+$("#favorite #place_ml").text(post.place_ml);
+$("#favorite #place_en").text(post.place_en);
+$("#favorite #description").text(post.description);
+$("#favorite #thumbnail").attr("src", post.thumbnail);
+$("#favorite #duration").text(post.duration);
+$("#favorite #duration").text(post.duration);
+$("#favorite #size").text(post.size);
 
     $("#tab-link").click(function(){
         const about = document.getElementById("about-tab");
@@ -46,7 +63,12 @@ $("#size").text(post.size);
         about.setAttribute('aria-selected', false);
         contact.setAttribute('aria-selected', true);
     });
-    $("#star").click(function(){
+    $("#post #star").click(function(){
+        const sc= $("#star i").attr("class");
+        var star= sc == "uil uil-favorite" ? "uis uis-favorite" : "uil uil-favorite" ;
+        $("#star i").attr("class", star);
+    });
+    $("#favorite #star").click(function(){
         const sc= $("#star i").attr("class");
         var star= sc == "uil uil-favorite" ? "uis uis-favorite" : "uil uil-favorite" ;
         $("#star i").attr("class", star);
